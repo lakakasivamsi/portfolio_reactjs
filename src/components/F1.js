8d5722db493fe785158820fc6e24d40a9de4ea08
lakakasivamsi <94736814+lakakasivamsi@users.noreply.github.com>
2023-10-15 10:15:49 +0530

    Add files via upload

---
+++ src/components/F1.js
@@ -0,0 +1,80 @@
+import './style.css';
+function F1(){
+    return(
+        <div></div>
+    )
+}
+export function Home()
+{
+return <div class="vas"><p>My  self Vamsi, I am currently pursuing my B.Tech from VIT-AP University. I am member of CSI group.
+My goal is to build a successful career.My strength is my positive attitude and weakness is my emotional Nature. Hobbies are playing games, reading, listening music. That's all about me.
+</p></div>
+}
+
+
+export function About()
+{
+return <div class="vas"><p>i am currently pursuing a Bachelor of Technology (B.Tech) at VIT-AP University, is a diligent scholar on a path of continuous learning and growth. His academic journey has been marked by excellence, having completed his intermediate education sadhana Junior College, and earlier, his secondary education at Narayana e-Techno Schools ,securing impressive percentages that reflect his dedication and commitment to his education.</p><table><tr><th>Studying</th><th>College Name</th><th>Percenatge or Grade</th></tr><tr><td>Schooling</td><td>Narayana e-techno Schools</td><td>10.Cgpa</td></tr><tr><td>Intermediate</td><td>Narayana Junior College</td><td>88.6%</td></tr><tr><td>B-tech</td><td>VIT-AP University</td><td>8.08 Cgpa *</td></tr></table></div>
+}
+export function Blog()
+{
+    return <div class="vas">
+            <h2>Skills</h2>
+            <div className="skill-box">
+            <div class="d-flex flex-row justify-content-center">
+            <div className="language-box">
+               
+                <h5>Java</h5>
+            </div>
+            <div className="language-box">
+               
+                <h5>Python</h5>
+            </div>
+            <div className="language-box">
+                
+                <h5>HTML</h5>
+            </div>
+            </div><br/>
+            <div class="d-flex flex-row justify-content-center">
+            <div className="language-box">
+                
+                <h5>CSS</h5>
+            </div>
+            <div className="language-box">
+                
+                <h5>JavaScript</h5>
+            </div>
+            <div className="language-box">
+                
+                <h5>React.js</h5>
+            </div>
+            </div><br/>
+            <div class="d-flex flex-row justify-content-center">
+            <div className="language-box">
+                
+                <h5>R<br/>Language</h5>
+            </div>
+            <div className="language-box">
+                
+                <h5>Android<br></br>Development</h5>
+            </div>
+            </div>
+            </div>
+        </div>
+}
+export function Contact()
+{
+return <div class='vas'>
+<div class="pran">
+
+    title="portfoli in html css"<br/>
+    title="bugenting tool cloud computing"<br/>
+    
+</div>
+<div class="pran">
+
+</div>
+</div>
+}
+
+export default F1;
